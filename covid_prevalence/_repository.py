fca55555d5b5e66f3c5cdd5cb2581aecb51a578a
Steven Horn <horn@horndev.com>
2020-07-07 09:15:49 -0300

    added gitpush to repository private module

--- covid_prevalence/_repository.py
+++ covid_prevalence/_repository.py
@@ -2,5 +2,12 @@
 Handle loading/saving data using Git repository
 '''
 
+import git
+
+def gitpush(message):
+  repo = git.Repo('/content/covid-prevalence')
+  repo.git.add('--all')
+  repo.git.commit('-m', message, author='Steven Horn')
+  repo.remotes.gitlab.push()
 
 
